4fd0ce3ea2b8aa76e755d2822c2cf653c687d51a
James Galantino <james_galantino@aol.com>
2021-06-28 10:45:59 -0400

    Initialized

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://jamesG116:sushi8585@cluster0.emohy.mongodb.net/merngSocialAppDB?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
